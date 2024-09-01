0220d162cf17deff095a3918cfd74f20773605a7
nataliya-panina <nataliya.sabathier@gmail.com>
2024-09-01 22:05:03 +0200

    Create variables.tf

---
+++ infra-cloud/variables.tf
@@ -0,0 +1,7 @@
+  GNU nano 6.2                                                     variables.tf                                                               
+variable "yc_token" {}
+
+variable "zone" {
+        default = "ru-central1-a"
+        description = "YaCloud network zone"
+}
